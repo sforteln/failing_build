322d7529ad97f35cc998a265dc17b89bae6ad56a
Fortelny, Simon (Contractor) <Simon.Fortelny@exchange.nordstrom.com>
2013-11-09 20:56:40 -0800

    initial commit

---
+++ src/main/java/com/failing/build/App.java
@@ -0,0 +1,13 @@
+package com.failing.build;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
